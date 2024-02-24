427223731c4495c6c3766a41129fac34e9969d99
André Gustavo <andrewinstitucional@gmail.com>
2024-02-24 16:37:52 -0300

    feat(show-all-items): Created show-all-items test

---
+++ README.md
@@ -0,0 +1,20 @@
+# Sistema
+Link: https://www.saucedemo.com/
+
+# Objetivo
+
+Trabalho do 5° Período da disciplina de Testes e Qualidade de Software cujo objetivo é realizar uma automatização de teste em um site chamado Sauce Demo utilizando RPAs, como o Selenium.
+
+Há no total 4 módulos (Menu, Inventory, Carrinho e Login). Cada membro da equipe terá exatamente 5 cenários, com seus respectivos casos de teste, como proposto pelo professor. 
+
+# Alunos
+- André Gustavo Xavier dos Santos
+- Lucas Gabriel Pereira Pestana
+- Marcus Fernando Santos Cuba
+- Miguel Farias Ramos Araújo
+
+#  Divisão da Equipe
+- **Menu**: André Gustavo
+- **Inventory**: Lucas Gabriel
+- **Carrinho**: Marcus Fernando 
+- **Login**: Miguel Farias
---
+++ menu/show-all-items.py
@@ -0,0 +1,48 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+# Objective: Create the Webdriver Instance, Login, Go to /cart Route, then get back to the /inventory Route
+
+# Prevent Webdriver instance from closing automatically
+options = webdriver.ChromeOptions()
+options.add_experimental_option('detach', True)
+
+service = Service(ChromeDriverManager().install())
+browser = webdriver.Chrome(service=service, options=options)
+browser.maximize_window()
+
+browser.get('https://www.saucedemo.com/')
+
+# Part 1 - Login
+browser.find_element(By.ID, 'user-name').send_keys('standard_user')
+time.sleep(1)
+browser.find_element(By.ID, 'password').send_keys('secret_sauce')
+time.sleep(1)
+browser.find_element(By.ID, 'login-button').click()
+time.sleep(1)
+
+# Part 2 - Clicking On The Cart Icon
+browser.find_element(By.CLASS_NAME, 'shopping_cart_link').click()
+time.sleep(1)
+
+# Part 3 - Opening Menu and Clicking in the Button "ALl Items"
+browser.find_element(By.ID, 'react-burger-menu-btn').click()
+time.sleep(1)
+browser.find_element(By.ID, 'inventory_sidebar_link').click()
+time.sleep(3)
+
+# Part 4 - Condition of the Route /inventory
+current_url = browser.current_url
+expected_url = 'https://www.saucedemo.com/inventory.html'
+
+if current_url == expected_url:
+    browser.execute_script("alert('The user is in the correct route.')")
+else:
+    browser.execute_script("alert('The user is not in the correct route.')")
+
+time.sleep(3)
+browser.quit()
